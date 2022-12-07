e3fd27a42fe41909ca46558bbaa03ee323d5a539
HYUKJU NA <mate_of_wind@naver.com>
2022-12-07 23:11:13 +0900

    초기 커밋.

---
+++ app.py
@@ -0,0 +1,29 @@
+from email.policy import default
+import math
+from flask import Flask, request, url_for, render_template, jsonify, redirect
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/input')
+def input():
+    return render_template('module1.html')
+
+@app.route('/post1', methods=['POST'])
+def cal():
+    global data1
+    data1 = int(request.form['data1'])
+    data1 = math.pow(data1,2)
+    return render_template('module2.html')
+
+@app.route('/post2', methods=['POST'])
+def cal2():
+    data2 = int(request.form['data2'])
+    data2 = math.pow(data1,data2)
+    return str(data2)
+
+@app.route('/img')
+def myimage():
+    return render_template("image1.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
