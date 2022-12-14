2b5f8227fb91daa261fe7c1c53d7f4526b9f4811
HYUKJU NA <mate_of_wind@naver.com>
2022-12-15 02:23:52 +0900

    json 입출력

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from email.policy import default
 import math
+import json
 from flask import Flask, request, url_for, render_template, jsonify, redirect
 app = Flask(__name__)
 
@@ -29,6 +30,19 @@ def myimage():
 def listprint():
     data = [[1,2,3],[4,5,6],[7,8,9]]
     return render_template('listprint.html', data=data)
+
+@app.route('/postman', methods=['POST'])
+def postman():
+    data = [[0 for j in range(6)] for i in range(3)]
+    input = request.get_json()
+    for i in range(2):
+        data[i][0] = input[i]['data1']
+        data[i][1] = input[i]['data2']
+        data[i][2] = input[i]['data3']
+        data[i][3] = input[i]['data4']
+        data[i][4] = input[i]['data5']
+        data[i][5] = input[i]['data6']
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
